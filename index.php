352fd1f7a1387102b84dd87d898aec4f80173adf
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 09:30:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  mail.log search
+  by SZabolcs Jászberényi
+  created in 2025
+
+  mail.log search is a web based tool to make searching in mail.log files easier.
+*/
+  require_once("system/config.php") || die("No configuration (system/config.php) present!");
+  require_once("system/functions.php") || die("Missing required system files (system/functions.php)!");
+  require_once("system/action.php") || die("Missing required system files (system/action.php)!");
+  require_once("system/style.php") || die("Missing required system files (system/style.php)!");
+?>
+
