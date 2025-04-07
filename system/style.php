0ffcf9a84656136d30b0a8a6117d4e1959a5655f
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 10:07:38 +0200

    Update style.php

--- system/style.php
+++ system/style.php
@@ -1,4 +1,19 @@
 <?php
-  $out.='
+  $out.='<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>mail.log search</title>
+    <meta name="author" content="Jászberényi Szabolcs" />
+    <meta name="copyright" content="Készítette: Jászberényi Szabolcs" />
+    <meta name="robots" content="all" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="system/style.css?<?=filemtime("system/style.css"); ?>" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div>
+    </div>
+  </body>
+</html>
 ';
 ?>
