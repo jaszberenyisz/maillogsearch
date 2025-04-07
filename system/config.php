dc48811a3444bf7e22cc1e5a4834414af70c85eb
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 11:09:10 +0200

    Update config.php

--- system/config.php
+++ system/config.php
@@ -1,2 +1,9 @@
 <?php
+  // Log files directory
+  // Default: /var/log/
+  $_SESSION["cfg"]["logdir"]="/var/log/";
+  // Default mail.log file name
+  // Default: mail.log
+  $_SESSION["cfg"]["logfilename"]="mail.log";
 ?>
+
