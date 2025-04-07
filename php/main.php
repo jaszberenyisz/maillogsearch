17f2dc3efee146c3451105bdca0f999556a7035f
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 17:32:11 +0200

    Update main.php

--- php/main.php
+++ php/main.php
@@ -1,13 +1,21 @@
 <?php
+  // First filter
   $q="";
   if (isset($_POST["q"])) $q=$_POST["q"];
   elseif (isset($_GET["q"])) $q=$_GET["q"];
   if (strlen($q)<=1) $q="";
+  // Second filter
+  $q2="";
+  if (isset($_POST["q2"])) $q2=$_POST["q2"];
+  elseif (isset($_GET["q2"])) $q2=$_GET["q2"];
+  if (strlen($q2)<=1) $q2="";
+  // Search form
   $out.='
 <div id="control" class="row w-50">
   <div class="col">
     <form action="?" method="post" enctype="multipart/form-data">
       <input type="text" name="q" id="q" placeholder="'._search.'" value="'.$q.'" />
+      <input type="text" name="q2" id="q2" placeholder="'._search.'" value="'.$q2.'" />
       <input type="Submit" value="'._OK.'" />
     </form>
   </div><!-- class:col -->
@@ -17,13 +25,16 @@
 </div>
 ';
 // Log reading and filtering
+// Is there onyl one log file?
   if (!is_array($_SESSION["cfg"]["logfilename"]))
   {
+    // Only one logfile
     $fn=$_SESSION["cfg"]["logdir"].$_SESSION["cfg"]["logfilename"];
     $c=get_logfile($fn,$q);
   }
   else
   {
+    // Multiple logfiles
     $c=array();
     for ($i=0;$i<count($_SESSION["cfg"]["logfilename"]);$i++)
     {
@@ -36,13 +47,17 @@
   if (count($c)>0)
   {
     $out.='<div id="loglines">'."\n";
+    // Maximalize output lines
     $maxlines=count($c);
     if (((int)$_SESSION["cfg"]["maxlines"]>0)and($maxlines>(int)$_SESSION["cfg"]["maxlines"])) $maxlines=(int)$_SESSION["cfg"]["maxlines"];
+    // Lets print them
     for ($i=0;$i<$maxlines;$i++)
     {
+      // Some formating
       $ctxt=$c[$i];
       $ctxt=htmlspecialchars($ctxt);
       $ctxt=make_links($ctxt);
+      // Print
       $out.='
   <div class="logline row">
     <div class="loglinecontent col"><div class="loglinenumber info">'.(int)($i+1).'</div> '.$ctxt.'</div>
