616d61c1d65a97014565974f1b46b9019b5d65d9
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 10:25:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 
   mail.log search is a web based tool to make searching in mail.log files easier.
 */
-  require_once("system/config.php") || die("No configuration (system/config.php) present!");
-  require_once("system/functions.php") || die("Missing required system files (system/functions.php)!");
-  require_once("system/action.php") || die("Missing required system files (system/action.php)!");
+  (require_once("system/config.php")) || die("No configuration (system/config.php) present!");
+  (require_once("system/functions.php")) || die("Missing required system files (system/functions.php)!");
+  (require_once("system/action.php")) || die("Missing required system files (system/action.php)!");
   $out="";
-  require_once("system/style.php") || die("Missing required system files (system/style.php)!");
+  (require_once("system/style.php")) || die("Missing required system files (system/style.php)!");
   print($out);
 ?>
