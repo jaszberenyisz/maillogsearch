386f69888b0dca07290fd712bc0f905c83595d3e
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 14:35:13 +0200

    Update config.php

--- system/config.php
+++ system/config.php
@@ -2,11 +2,17 @@
   // Log files directory
   // Default: /var/log/
   $_SESSION["cfg"]["logdir"]="/var/log/";
+  
   // Default mail.log file name
   // Default: mail.log
   // you can specify more than one filename on the same location
   // Or leave the logdir parameter empty and use full path filenames instead
 //  $_SESSION["cfg"]["logfilename"]="mail.log";
   $_SESSION["cfg"]["logfilename"]=array("mail.log.1","mail.log");
+
+  // Maximum lines to show on the web interface
+  // 0 means no limit
+  // Be carefull with no limit as it can consume a huge amount of memory on the client side too!
+  $_SESSION["cfg"]["maxlines"]="200";
 ?>
 
