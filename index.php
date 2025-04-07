d2da9f5c7b4a9fda7e8f6026eacacee9125288b7
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 11:09:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
   mail.log search is a web based tool to make searching in mail.log files easier.
 */
+  if (session_id()=="") session_start();
   (require_once("system/config.php")) || die("No configuration (system/config.php) present!");
   (require_once("system/functions.php")) || die("Missing required system files (system/functions.php)!");
   (require_once("system/action.php")) || die("Missing required system files (system/action.php)!");
