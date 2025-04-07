e206a5c607080e5de6a646fd6f17e626de923e7a
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 11:57:36 +0200

    Update style.php

--- system/style.php
+++ system/style.php
@@ -8,6 +8,7 @@
     <meta name="copyright" content="Készítette: Jászberényi Szabolcs" />
     <meta name="robots" content="all" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="lib/bootstrap-5.1.3-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="system/style.css?'.filemtime("system/style.css").'" rel="stylesheet" type="text/css" />
   </head>
   <body>
