c8716a36684796067f29aebd1808c8b2c1162cec
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 10:33:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
   (require_once("system/config.php")) || die("No configuration (system/config.php) present!");
   (require_once("system/functions.php")) || die("Missing required system files (system/functions.php)!");
   (require_once("system/action.php")) || die("Missing required system files (system/action.php)!");
+  $a="main";
+  $fn=$a.".php";
+  $out="";
+  if (file_exists("php/".$fn))
+  {
+    require_once("php/".$fn));
+    $body=$out;
+  }
+  else die("Missing required system files (php/".$fn.")!");
   $out="";
   (require_once("system/style.php")) || die("Missing required system files (system/style.php)!");
   print($out);
