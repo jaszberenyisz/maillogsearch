8ffa1ed5bd9d8e75115ea0ea36ca4d7e67139a08
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-07 12:39:42 +0200

    Update functions.php

--- system/functions.php
+++ system/functions.php
@@ -1,2 +1,30 @@
 <?php
+// --------------------------------------------------------------------
+// Get contents of lofile with filtering
+function get_logfile($fn,$q)
+{
+  global $out;
+  if (file_exists($fn))
+  {
+    $out.='<p>Log file: '.$fn."</p>\n";
+    $f=fopen($fn,"r");
+    if ($f)
+    {
+      $c=array();
+      while (($line = fgets($f)) !== false)
+      {
+        if (isset($q))
+        {
+          if (strstr($line,$q)==true) $c[]=$line;
+        }
+        else $c[]=$line;
+      }
+      fclose($f);
+    }
+    else $out.=' <p>Error reading file: '.$fn."</p>\n";
+  }
+  else $out.='<p>File does not exists: '.$fn.'</p>';
+  return $c;
+}
+// --------------------------------------------------------------------
 ?>
