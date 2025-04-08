4f3b78b05ada55cd4e0a75c97026e9b956ca36ed
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-08 14:33:27 +0200

    Update functions.php

--- system/functions.php
+++ system/functions.php
@@ -39,17 +39,19 @@ function make_links($c)
   // E-mail IDs
   $c=preg_replace('/]\: ([A-Z0-9]{10})\:/', ']: <a href="?q=$1">$1</a>: ', $c);
   // queue as IDs
-  $c=preg_replace('/queued as (.*)\)/', 'queued as <a href="?q=$1">$1</a>)', $c);
+  $c=preg_replace('/queued as ([A-Z0-9]{10})/', 'queued as <a href="?q=$1">$1</a>', $c);
   // forward as IDs
-  $c=preg_replace('/forwarded as (.*)\)/', 'forwarded as <a href="?q=$1">$1</a>)', $c);
+  $c=preg_replace('/forwarded as ([A-Z0-9]{10})/', 'forwarded as <a href="?q=$1">$1</a>', $c);
   // E-mail addresses
   $c=preg_replace('/\&lt\;([^\&]*)/', '&lt;<a href="?q=$1">$1</a>', $c);
   // Normal status
   $c=preg_replace('/Passed CLEAN/', '<span class="msg_green">Passed CLEAN</span>', $c);
   $c=preg_replace('/250 2\.0\.0 Ok/', '<span class="msg_green">250 2.0.0 Ok</span>', $c);
   // Error status
-  $c=preg_replace('/removed/', '<span class="msg_red">removed</span>', $c);
   $c=preg_replace('/authentication failed: /', '<span class="msg_red">authentication failed</span>: ', $c);
+  $c=preg_replace('/lost connection after /', '<span class="msg_red">lost connection</span> after ', $c);
+  $c=preg_replace('/removed/', '<span class="msg_red">removed</span>', $c);
+  $c=preg_replace('/SSL_accept error from /', '<span class="msg_red">SSL_accept error</span> from ', $c);
   $c=preg_replace('/unknown user: /', '<span class="msg_red">unknown user</span>: ', $c);
   return $c;
 }
