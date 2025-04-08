7d763bd98d7f70228738eb31557ee4720fa07312
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-04-08 13:48:06 +0200

    Update hu.php

--- lang/hu.php
+++ lang/hu.php
@@ -4,6 +4,7 @@ Hungarian language file
 created by Szabolcs Jászberényi <info at jszabolcs dot hu>
 created in 2025
 */
+  define("_lines_shown","sort mutat");
   define("_OK","OK");
   define("_search","Keresés...");
 ?>
