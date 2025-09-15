c083e0e60521000cb6e26a0418401fad000f2821
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-15 12:22:09 +0200

    Update config.php
    Added custom keywords parameter

--- system/config.php
+++ system/config.php
@@ -19,6 +19,11 @@
   // Be carefull with no limit as it can consume a huge amount of memory on the client side too!
   $_SESSION["cfg"]["maxlines"]="5000";
 
+  // Custom keywords
+  // Array of static keywords for quicksearch
+  // Example: $_SESSION["cfg"]["keywords"]=array("one","two","email@somewhere.com","error");
+  $_SESSION["cfg"]["keywords"]=array();
+
   // Language selection
   // Default: english (en)
   $_SESSION["cfg"]["language"]="en";
