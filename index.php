e8c129e667689ed1b03e4bcf667568b0241b57d7
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-17 09:59:56 +0200

    Update index.php
    Added config_auto.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 */
   if (session_id()=="") session_start();
   (require_once("system/config.php")) || die("No configuration (system/config.php) present!");
+  (require_once("system/config_auto.php")) || die("No automatic configuration (system/config_auto.php) present!");
   (require_once("lang/".$_SESSION["cfg"]["language"].".php")) || die("No language file (lang/".$_SESSION["cfg"]["language"].".php) present!");
   (require_once("system/functions.php")) || die("Missing required system files (system/functions.php)!");
   (require_once("system/action.php")) || die("Missing required system files (system/action.php)!");
