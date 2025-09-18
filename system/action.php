0619ca746162c9c850d8b3d809921c0efdb097fc
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-18 12:43:30 +0200

    Update action.php

--- system/action.php
+++ system/action.php
@@ -1,2 +1,26 @@
 <?php
+/*
+  mail.log search
+  by Szabolcs Jászberényi <info at jszabolcs dot hu>
+  created in 2025
+
+  mail.log search is a web based tool to make searching in mail.log files easier.
+*/
+  if (isset($_GET["muvelet"])) $muvelet=$_GET["muvelet"];
+  elseif (isset($_POST["muvelet"])) $muvelet=$_POST["muvelet"];
+  else $muvelet="";
+  switch ($muvelet)
+  {
+// Automatikus frissítési időzítő bekapcsolása
+    case "auto_refresh_on":
+      $_SESSION["refresh"]=1;
+      break;
+// Automatikus frissítési időzítő kikapcsolása
+    case "auto_refresh_off":
+      $_SESSION["refresh"]=0;
+      break;
+// Default művelet ha hiba van
+    default:
+      break;
+  }
 ?>
