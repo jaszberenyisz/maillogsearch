05159aa6d422bf62b5566cec3bf9497784e43558
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-18 12:39:41 +0200

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -4,6 +4,8 @@ English language file
 created by Szabolcs Jászberényi <info at jszabolcs dot hu>
 created in 2025
 */
+  define("_auto_refresh_off","Automatic refresh off");
+  define("_auto_refresh_on","Automatic refresh on");
   define("_newest_on_top","Newest on top");
   define("_lines_shown","lines shown");
   define("_off","Off");
@@ -11,5 +13,6 @@ created in 2025
   define("_OK","OK");
   define("_oldest_on_top","Oldest on top");
   define("_search","Search...");
+  define("_status","Status");
   define("_quick_search","Quick Search...");
 ?>
