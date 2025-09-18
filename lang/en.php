1b23c78f9b55dfdc5eed4a6a4147ea2da0246e2b
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-18 12:33:49 +0200

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -6,6 +6,8 @@ created in 2025
 */
   define("_newest_on_top","Newest on top");
   define("_lines_shown","lines shown");
+  define("_off","Off");
+  define("_on","On");
   define("_OK","OK");
   define("_oldest_on_top","Oldest on top");
   define("_search","Search...");
