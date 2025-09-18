f615ec61c50ae60b64b693ce27b64cc147c78c2e
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-18 12:34:03 +0200

    Update hu.php

--- lang/hu.php
+++ lang/hu.php
@@ -6,6 +6,8 @@ created in 2025
 */
   define("_newest_on_top","Legújabb előre");
   define("_lines_shown","sort mutat");
+  define("_off","Ki");
+  define("_on","Be");
   define("_OK","OK");
   define("_oldest_on_top","Legrégebbi előre");
   define("_search","Keresés...");
