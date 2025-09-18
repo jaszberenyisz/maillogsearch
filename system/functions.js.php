76820f9ddb436e4a2236110f2fe73895968e6d58
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-09-18 11:39:42 +0200

    Create functions.js.php

---
+++ system/functions.js.php
@@ -0,0 +1,24 @@
+<?php
+  if (session_id()=="") session_start();
+?>
+function ajax(x,reload=false,clear=false)
+{
+  if ((reload==false)||(clear==true)) { clearTimeout(ajaxReload); }
+  var xhttp = newxhttp();
+  xhttp.open("GET","ajax.php?f="+x+"&amp;PHPSESSID=<?=session_id(); ?>",true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState==4 && this.status==200)
+    {
+      var e = document.getElementById("content");
+      e.innerHTML = this.responseText;
+      if (reload==true) { ajaxReload=setTimeout("ajax('"+x+"',"+reload+");",<?=(int)($_SESSION["refresh"]*1000); ?>); }
+    }
+  }
+  xhttp.send(null);
+}
+function newxhttp()
+{
+  if (window.XMLHttpRequest) xhttp = new XMLHttpRequest();
+  else if (window.ActiveXObject) xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  return xhttp;
+}
